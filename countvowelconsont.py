0eb381464bec85887d28376ac8b3b2b96d5bbd3f
Rinku <Krinku33@gmail.com>
2022-10-11 08:55:46 +0100

    goo hero

---
+++ countvowelconsont.py
@@ -0,0 +1,12 @@
+a=input("enter a thing\n")
+vowel=0
+cons=0
+for i in range(0,len(a)):
+    if(a[i]!=""):
+        if(a[i]=="a" or a[i]=="e" or a[i]=="i" or a[i]=="o" or a[i]=="u" or a[i]=="A" or a[i]=="E"
+        or a[i]=="I" or a[i]=="O" or a[i]=="u"):
+            vowel=vowel+1
+        else:
+            cons=cons+1
+print("vowels=",vowel)
+print("consonents=",cons)
